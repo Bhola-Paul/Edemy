6de01e60d9afdefd522e02d4f33733cafce7668b
Bhola Paul <paulbhola1987@gmail.com>
2025-04-02 00:10:08 +0530

    backend

---
+++ server/middlewares/auth.middleware.js
@@ -0,0 +1,17 @@
+import { clerkClient } from "@clerk/express";
+
+//middleware 
+export const protectEducator=async (req,res,next) => {
+    try {
+        const userId=req.auth.userId
+        // console.log(userId);
+        const response=await clerkClient.users.getUser(userId);
+        // console.log(response.privateMetadata);
+        if(response.privateMetadata.role !== 'educator'){
+            return res.json({success:false,message:'Unauthorized Access'});
+        }
+        next();
+    } catch (error) {
+        res.json({success:false,message:`midleware ${error.message}`});
+    }
+} 
--- server/server.js
+++ server/server.js
@@ -3,19 +3,30 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js';
 import { clerkWebhooks } from './controllers/webhooks.js';
+import educatorRouter from './routes/educator.routes.js';
+import { clerkMiddleware } from '@clerk/express';
+import connectCloudinary from './configs/cloudinary.js';
+import courseRouter from './routes/course.routes.js';
+import userRouter from './routes/user.routes.js';
+
 
 //Initialize express
 const app=express();
 
 //connect to database
 await connectDB();
+await connectCloudinary();
 
 //middlewares
 app.use(cors());
+app.use(clerkMiddleware());
 
 // Routes
 app.get('/',(req,res)=> res.send('API working!'))
 app.post('/clerk',express.json(),clerkWebhooks);
+app.use('/api/educator',express.json(),educatorRouter);
+app.use('/api/course',express.json(),courseRouter)
+app.use('/api/user',express.json(),userRouter);
 
 //port
 const PORT=process.env.PORT || 5000;
